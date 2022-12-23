3b4a077a2cc76d3b9916d9cbd41280bdce348fa1
KimJinHyeon0 <rlawlsgus117@gmail.com>
2022-12-23 13:03:11 +0900

    [ADD] attributes

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 class Config:
-    num_delivery = 15
-    num_pro = 3
+    num_delivery = 100
+    num_pro = 20
 
     id_length_delivery = 5
     id_length_pro = 3
@@ -18,16 +18,20 @@ class Config:
     max_start_time = 1200  # 8pm
     min_start_time = 540  # 9am
 
-    max_elapsed_time = 60  # minute
+    max_elapsed_time = 60  # min
     min_elapsed_time = 20
 
-    max_distance = 5000  # miter
-    min_distance = 500
+    max_distance = 5  # km
+    min_distance = 0.5
 
-    max_pickup_time = 60  # minute
+    max_pickup_time = 60  # min
     min_pickup_time = 15
 
     # Pro associated
     max_capacity = 100
     min_capacity = 50
+
+    # others
+    speed_limit = 40  # km/h
+    delivery_interval = 5  # min
 
--- main.py
+++ main.py
@@ -2,11 +2,50 @@ from configs import Config
 from utils import *
 
 
+@dataclass
+class Delivery:
+    id: str = field(default_factory=generate_id_delivery)
+
+    src_lon: float = field(default_factory=generate_lon)
+    src_lat: float = field(default_factory=generate_lat)
+
+    dst_lon: float = field(init=False)
+    dst_lat: float = field(init=False)
+
+    distance: float = field(init=False)
+
+    baggage: int = field(default_factory=generate_baggage)
+
+    start_time: int = field(default_factory=generate_start_time)
+    interval_time: int = field(default_factory=generate_elapsed_time)
+    end_time: int = field(init=False)
+    estimated: int = field(init=False)
+
+    assigned: str = field(default_factory=str)
+
+    def __post_init__(self):
+        self.dst_lon, self.dst_lat = generate_random_coor(self.src_lon, self.src_lat, self.interval_time)
+        self.distance = haversine(self.src_lon, self.src_lat, self.dst_lon, self.dst_lat)
+        self.end_time = self.start_time + self.interval_time
+        self.estimated = distance2time(self.distance)
+
+
+@dataclass
+class Pro:
+    id: str = field(default_factory=generate_id_pro)
+
+    pre_lon: float = field(default_factory=generate_lon)
+    pre_lat: float = field(default_factory=generate_lat)
+
+    max_cap: int = field(default_factory=generate_capacity)
+    cur_cap: int = field(default=0)
+
+    completed: list = field(default_factory=list)
+
+
 def main() -> None:
-    delivery_list = [Delivery() for _ in range(Config.num_delivery)]
-    pro_list = [Pro() for _ in range(Config.num_pro)]
-    df_delivery = pd.DataFrame.from_dict(delivery_list)
-    df_pro = pd.DataFrame.from_dict(pro_list)
+    df_delivery = pd.DataFrame.from_dict([Delivery() for _ in range(Config.num_delivery)])
+    df_pro = pd.DataFrame.from_dict([Pro() for _ in range(Config.num_pro)])
     visualize(df_delivery, df_pro)
 
 
